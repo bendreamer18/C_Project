95429ab5608992cb8d272e896eb9ee841d45dbba
almosthere <luna.dolinin@yandex.ru>
2016-12-18 16:06:15 +0300

    Добавлены все марки автомобилей. Пробую добавить пункт Цена

--- op_project.c
+++ op_project.c
@@ -6,9 +6,9 @@
 
 // Программа обработки списка, в которой используется массив структур.
 
-#define MAX 10
+#define MAX 101
 #define LEN 15
-int ger = 0, cursorLarge = 0, cursorSmall = 0;
+int ger = 0, cursorLarge = 0, cursorSmall = 0, wheat = 0;
 // Это структура для общего списка товаров
 struct item {
 		char type[30]; // Тип товара
@@ -102,8 +102,11 @@ void enter(void)
 	char s[80];
 	
 	slot = find_free();
-	
- //Работаем с подгрузкой файла	
+
+//Функция для считывания данных из файлов и их запись в массив структур	
+ int upload_files() {
+ 	
+//Подгрузка файлов 	
 char typesArray[LEN];
 char marksArray[LEN]; 
 char namesArray[LEN];
@@ -118,7 +121,7 @@ char daysArray[LEN];
         
 //        sc(fileTypes) - количество строк в файле
 		int m;
-	
+// Это условие перемещает внутренний указатель файлов для последовательного чтения строк	
 		if(ger == 0) {
 			fseek( fileTypes , 0 , SEEK_SET);
 			fseek( fileMarks , 0 , SEEK_SET);
@@ -157,19 +160,14 @@ char daysArray[LEN];
  			fgets(daysArray, 5, fileDays);   
  			item_list[slot].delivery = strtoul(daysArray, NULL, 10);
  			
-  
-//			if (ger != 0) {
-//			fseek( fileTypes , 15 , SEEK_SET);
-//			fseek( fileMarks , 15 , SEEK_SET);
-//			fseek( fileNames , 15 , SEEK_SET);
-//			fseek( fileStock , 5 , SEEK_SET);
-//			fseek( fileDays , 5 , SEEK_SET);
-//			}
-
+ 		wheat+=2;
         fclose(fileTypes);
+ 		return 0;
+ }
 	
+upload_files();
 	
-
+// Добавление товаров в массив структур вручную 
 //	printf("Введите тип товара: ");
 //	gets(item_list[slot].type);
 //	
@@ -186,9 +184,6 @@ char daysArray[LEN];
 //	printf("Введите дни на доставку: ");
 //	gets(s);
 //	item_list[slot].delivery = strtoul(s, '\0', 10);
-//	 Работает для присвоения
-//	strcpy(item_list[slot].type , "Egzona");
-//	printf("%s", item_list[slot].type);
 }
 // Поиск свободной структуры
 int find_free(void) 
@@ -215,15 +210,25 @@ void delete(void)
 	}
 // Вывод списка на экран
 void list(void) 
-{ 
-	printf("\n№\tТип товара\t\tМарка авто\t\tНаименование товара\t\tВ наличии(шт)\t\tСроки доставки(дн)\n\n");
+{ 	if(wheat == 2) {
+	printf("\n№\tТип товара\t\tМарка авто\tНаименование товара\t\tВ наличии(шт)\t\tСроки доставки(дн)\n\n");
+} else {
+	printf("\n№\tТип товара\t\t\tМарка авто\t\tНаименование товара\t\tВ наличии(шт)\t\tСроки доставки(дн)\n\n");
+}
+	
 	register int t;
 	
 	for(t = 0; t < MAX; ++t) {
 		if(item_list[t].name[0]) {
 			printf("%d.\t", t);
-			printf("%s\t\t", item_list[t].type);
-			printf("%s\t\t", item_list[t].mark);
+			if(wheat == 2) {
+				printf("%s\t", item_list[t].type);
+				printf("%s\t", item_list[t].mark); //хуета с отступами при вызове функции
+			}
+			else {
+				printf("%s\t\t\t", item_list[t].type);
+				printf("%s\t\t\t", item_list[t].mark);
+			}
 			printf("%s\t\t\t", item_list[t].name);
 			printf("%d\t\t\t", item_list[t].have);
 			printf("%d\n", item_list[t].delivery);
